62ae07856b38790bdebb85e795c2a4a1bcacb96b
Jeremías <jeremias.rootstrap@gmail.com>
2025-11-07 13:44:07 -0300

    some code doc

--- sbin/system-upgrade.sh
+++ sbin/system-upgrade.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -eux
+
+# macOS
 sudo softwareupdate -a -i
+
+# Mac App Store
 mas outdated
 mas upgrade
+
+# brew
 brew update
 brew outdated --greedy
 brew upgrade --greedy
-exit 0
